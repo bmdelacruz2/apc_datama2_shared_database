3cd1ad0e3227a196174742129a5cd75f5ad41c5b
delacruzbri712 <52224604+delacruzbri712@users.noreply.github.com>
2019-12-19 14:29:15 +0800

    uploaded views screenshot

---
+++ projects3_oracle_view/view/views-query.sql
@@ -0,0 +1,20 @@
+CREATE VIEW MI182_BMDELACRUZ2.warehouses_inner_join_view 
+AS SELECT MI182_BMDELACRUZ2.warehouses.id, MI182_BMDELACRUZ2.warehouses.street_name, MI182_BMDELACRUZ2.warehouses.district, MI182_BMDELACRUZ2.warehouses.postal_code, MI182_BMDELACRUZ2.warehouses.province, MI182_BMDELACRUZ2.warehouses.country, MI182_BMDELACRUZ2.warehouse_types.warehouse_type 
+FROM MI182_BMDELACRUZ2.warehouses 
+INNER JOIN MI182_BMDELACRUZ2.warehouse_types on MI182_BMDELACRUZ2.warehouses.id = MI182_BMDELACRUZ2.warehouse_types.id 
+ORDER BY MI182_BMDELACRUZ2.warehouses.id ASC;
+
+
+CREATE VIEW MI182_BMDELACRUZ2.warehouses_inner_join_view 
+AS SELECT MI182_BMDELACRUZ2.warehouses.id, MI182_BMDELACRUZ2.warehouses.street_name, MI182_BMDELACRUZ2.warehouses.district, MI182_BMDELACRUZ2.warehouses.postal_code, MI182_BMDELACRUZ2.warehouses.province, MI182_BMDELACRUZ2.warehouses.country, MI182_BMDELACRUZ2.warehouse_types.warehouse_type 
+FROM MI182_BMDELACRUZ2.warehouses 
+INNER JOIN MI182_BMDELACRUZ2.warehouse_types on MI182_BMDELACRUZ2.warehouses.id = MI182_BMDELACRUZ2.warehouse_types.id 
+ORDER BY MI182_BMDELACRUZ2.warehouses.id ASC;
+
+
+CREATE VIEW MI182_BMDELACRUZ2.warehouses_inner_join_view 
+AS SELECT MI182_BMDELACRUZ2.warehouses.id, MI182_BMDELACRUZ2.warehouses.street_name, MI182_BMDELACRUZ2.warehouses.district, MI182_BMDELACRUZ2.warehouses.postal_code, MI182_BMDELACRUZ2.warehouses.province, MI182_BMDELACRUZ2.warehouses.country, MI182_BMDELACRUZ2.warehouse_types.warehouse_type 
+FROM MI182_BMDELACRUZ2.warehouses 
+INNER JOIN MI182_BMDELACRUZ2.warehouse_types on MI182_BMDELACRUZ2.warehouses.id = MI182_BMDELACRUZ2.warehouse_types.id 
+ORDER BY MI182_BMDELACRUZ2.warehouses.id ASC;
+
